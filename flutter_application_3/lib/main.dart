e41d97c8fbb9fb0e47c816237d37c322ab4060f3
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2023-07-03 03:48:51 +0530

    update

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light,
         theme: ThemeData(
             primarySwatch: Colors.brown,
--- flutter_application_3/lib/widgets/drawer.dart
+++ flutter_application_3/lib/widgets/drawer.dart
@@ -5,14 +5,21 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imageUrl =
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
     return Drawer(
       child: ListView(
         children: [
           DrawerHeader(
-            margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              decoration: BoxDecoration(color: Colors.brown),
               child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
                   accountName: Text("Samar"),
-                  accountEmail: Text("sam123@gail.com")))
+                  accountEmail: Text("sam123@gail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageUrl),
+                  )))
         ],
       ),
     );
