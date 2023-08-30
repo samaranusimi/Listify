c6a50e2edfa16da6213b4bcfc24f43f6df913101
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2023-08-31 03:33:16 +0530

    update

--- flutter_application_3/lib/widgets/theme.dart
+++ flutter_application_3/lib/widgets/theme.dart
@@ -9,7 +9,8 @@ class MyTheme {
           color: Colors.white,
           elevation: 0.0,
           iconTheme: IconThemeData(color: Colors.black),
-          titleTextStyle: TextStyle(color: Colors.black)));
+          titleTextStyle: TextStyle(
+              color: Colors.black, fontSize: 23, fontWeight: FontWeight.bold)));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
