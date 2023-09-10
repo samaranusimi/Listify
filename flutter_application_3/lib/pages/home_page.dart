605ad7d1ce58d973df0449c00e31c8618cc0086d
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2023-09-10 15:50:32 +0530

    update

--- flutter_application_3/lib/pages/home_page.dart
+++ flutter_application_3/lib/pages/home_page.dart
@@ -1,23 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_3/models/catalog.dart';
 import 'package:flutter_application_3/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class Home_Page extends StatelessWidget {
+class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
 
   @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(title: Text("Catalog App")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index]);
+            return ItemWidget(item: CatalogModel.items[index]);
           },
         ),
       ),
