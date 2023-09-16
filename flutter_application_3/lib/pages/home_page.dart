08c372f635062c25c642c7ec081ccca19d923474
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2023-09-17 05:23:32 +0530

    update

--- flutter_application_3/lib/pages/home_page.dart
+++ flutter_application_3/lib/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_application_3/models/catalog.dart';
 import 'package:flutter_application_3/widgets/theme.dart';
-import 'package:velocity_x/velocity_x.dart';
 
 class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
@@ -41,10 +43,10 @@ class _Home_PageState extends State<Home_Page> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CatalogHeader(),
-              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              if (CatalogModel.items != Null && CatalogModel.items.isNotEmpty)
                 CatalogList().expand()
               else
-                Center(
+                const Center(
                   child: CircularProgressIndicator(),
                 )
             ],
@@ -88,22 +90,62 @@ class CatalogList extends StatelessWidget {
 
 class CatalogItem extends StatelessWidget {
   final Item catalog;
-  const CatalogItem({super.key, required this.catalog});
+  const CatalogItem({
+    Key? key,
+    required this.catalog,
+  })  : assert(catalog != Null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return VxBox(
         child: Row(
       children: [
-        Image.network(catalog.image)
-            .box
-            .rounded
-            .p8
-            .color(MyTheme.creamColor)
-            .make()
-            .p16()
-            .w40(context)
+        CatalogImage(image: catalog.image),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.bold.xl.make(),
+            catalog.desc.text.semiBold.make(),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              children: [
+                "\$${catalog.price}".text.bold.lg.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluish),
+                        shape: MaterialStateProperty.all(StadiumBorder())),
+                    child: "Buy".text.make())
+              ],
+            ).pOnly(right: 8)
+          ],
+        ))
       ],
-    )).white.square(150).make().py16();
+    )).white.square(150).rounded.make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  })  : assert(image != Null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
